9d26f16ab079350f655f3e5b84d7448208fa9a3e
vq2x3 ☑️ <104202899+vq3x2@users.noreply.github.com>
2023-12-06 18:41:10 +0100

    Update nova_v1.0.1.py

--- nova_v1.0.1.py
+++ nova_v1.0.1.py
@@ -4,6 +4,7 @@ import os
 import requests
 import random
 import string
+import concurrent.futures
 
 
 # Verwende den relativen Pfad zum Unterordner, um auf die Dateien zuzugreifen
@@ -14,18 +15,19 @@ with open(os.path.join("./version./version.txt"), "r") as version:
     ver_en = version.readline()
 
 
-banner = f'''{Fore.LIGHTBLUE_EX}
+
+banner = f"""{Fore.LIGHTBLUE_EX}
 
 
                                 ███╗   ██╗ ██████╗ ██╗   ██╗ █████╗ 
                                 ████╗  ██║██╔═══██╗██║   ██║██╔══██╗
                                 ██╔██╗ ██║██║   ██║██║   ██║███████║     made by vq3x2
-                                ██║╚██╗██║██║   ██║╚██╗ ██╔╝██╔══██║     last updated: {date_en}                                ██║ ╚████║╚██████╔╝ ╚████╔╝ ██║  ██║
+                                ██║╚██╗██║██║   ██║╚██╗ ██╔╝██╔══██║     version: {ver_en}                                
+                                ██║ ╚████║╚██████╔╝ ╚████╔╝ ██║  ██║
                                 ╚═╝  ╚═══╝ ╚═════╝   ╚═══╝  ╚═╝  ╚═╝
-                                '''
+                                """
 
 print(banner)
-
 
 leng = 16
 char = string.ascii_letters + string.digits
@@ -40,8 +42,14 @@ while True:
 
         count = 0
         t0 = time.time()
-        for ii in range(num):
-            code = "".join(random.choice(char)for i in range(leng))
+
+
+
+
+
+
+        
+        def send_request(code):
             link = f"https://discordapp.com/api/v6/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true"
 
             response = requests.get(link)
@@ -52,17 +60,28 @@ while True:
                 with open("./Codes./Valid Codes.txt", "a+", encoding='utf-8') as valid:
                     valid.write(f"https://discord.gift/{code}" + "\n")
 
-
-            if response.status_code == 404:
+            elif response.status_code == 404:
                 print(f"{Fore.BLUE}[~] Note: {Fore.MAGENTA}https://discord.gift/{code} {Fore.WHITE}| {Fore.CYAN}Invalid")
 
                 with open("./Codes./Invalid Codes.txt", "a+", encoding='utf-8') as invalid:
                     invalid.write(f"https://discord.gift/{code}" + "\n")
 
-            if response.status_code == 429:
+            elif response.status_code == 429:
                 print(f"{Fore.RED}[-] Error: {Fore.YELLOW}Banned from Server {Fore.WHITE}| {Fore.LIGHTRED_EX}Reason:", response.reason)
-                time.sleep(1)
-                count += 1
+                # time.sleep(0)
+                # count += 1
+
+        codes = ["".join(random.choice(char) for i in range(leng)) for _ in range(num)]
+
+        urls = [f"https://discord.gift/{code}" for code in codes]
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(send_request, codes)
+
+
+
+
+
 
         print()
         input(f"{Fore.LIGHTBLACK_EX}Checked {Fore.LIGHTRED_EX}{num-count}/{num} {Fore.LIGHTBLACK_EX}in {Fore.LIGHTRED_EX} {time.time()-t0:.2f} seconds {Fore.WHITE}| {Fore.LIGHTBLACK_EX}All codes saved in txt files")
